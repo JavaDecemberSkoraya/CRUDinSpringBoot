8177d038f126e7bf8cf7a661e4d883d6a821e2ee
AsyaSkoraya <153989033+AsyaSkoraya@users.noreply.github.com>
2024-04-16 21:38:31 +0500

    CRUD in the SpringBoot

---
+++ src/main/java/ru/skoraya/springProb/SpringProbApplication.java
@@ -0,0 +1,13 @@
+package ru.skoraya.springProb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringProbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/skoraya/springProb/SpringProbApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.skoraya.springProb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringProbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
